384199102a233fdd35e29b2b9bd346295465619d
Gurpreet Gandhi <gurpreetgandhi3@gmail.com>
2017-04-02 23:03:45 +0530

    Reverse a Link List

---
+++ Revese a link list.c
@@ -0,0 +1,96 @@
+/*
+					------------------Link List Functions in C-------------------------------------
+					-------------------Author: Gurpreet Gandhi(Garry)----------------------------
+					--------------------Date: 02/04/2017--------------------------------------------
+					===================Functions Used ===============================================
+					->Reverse(link list, index)
+					-> main
+					->Print
+ 	*/
+					//------create a data structure
+					
+					struct node{
+					int data;
+					struct node* link;
+					};
+					//====================add lib here=====================
+					#include<stdlib.h>
+					#include<conio.h>
+					#include<stdio.h>
+					//======================================================
+					Reverse( struct node **head){
+						struct node *a,*b,*c;
+						a=NULL;
+						b=*head;
+						c=NULL;
+						while(b!= NULL)
+						{
+							
+							c=b->link;
+							b->link=a;
+							a=b;
+							b=c;
+						}
+						*head=a;
+						return 0;
+					}
+					
+					
+						AddAtLast(struct node **head, int val){
+						struct node *a;
+						//====create a new node for this
+						struct node *new;
+						new=(struct node *)malloc(sizeof(struct node *));
+						new->data=val;
+						new->link=NULL; //---can be assigned as '\0'
+						//===traverse till last
+						a=*head;
+						while(a->link!=NULL){
+							a=a->link;
+						}
+						a->link=new;
+						return 0;
+					}
+					
+						Print(struct node **head){
+						
+						struct node *a;
+						a=*head;
+						printf(" \n  Start --");
+						do{
+							printf(" %d -- ",a->data);
+							a=a->link;
+						}while(a->link != NULL);
+						printf(" %d -- EOL ",a->data);
+						
+						return 0;
+						
+					}
+					
+					main(){
+						struct node *p, *ptr,**new;
+						//======allocate memory to the node
+						p=(struct node *)malloc(sizeof(struct nonde*));
+						ptr=(struct node *)malloc(sizeof(struct nonde*));
+						p->data=10;
+						ptr->data=20;
+						//======== create a link list of 2
+						p->link=ptr;
+						ptr->link=NULL;
+						//=========== Add a value of 30 at last
+						
+						AddAtLast(&p,30);
+						AddAtLast(&p,60);
+						//===========Reverse List
+						
+						Reverse(&p);
+						
+						//==========print list
+						
+						Print(&p);
+						
+						return 0;
+						
+						getch();
+					}
+	
